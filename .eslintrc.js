265ec7b08b9f99623c8387e6f202f35594c93242
ostap_poliakov <cortezos@yandex.ru>
2022-03-23 20:09:23 +0300

    init project and set up ESLint

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+  },
+  extends: ["airbnb", "plugin:prettier/recommended"],
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 12,
+  },
+  plugins: ["react"],
+  rules: {
+    "prettier/prettier": [
+      "error",
+    ],
+  },
+}
